c7fe27baaed2ddb400f71101ff64e3dc987550e7
AshokA <ashok.mandage22@gmail.com>
2020-08-06 20:51:54 +0530

    added store daily wage to total wage use case

---
+++ storeDailyWageWithTotalWage.sh
@@ -0,0 +1,52 @@
+#!/bin/bash -x
+isFullTime=1
+isPartTime=2;
+ratePerHour=20;
+empCheck=$((RANDOM%3))
+monthWorkingDays=20;
+addingWage=0
+totalEmpWorkDays=0
+
+
+        function getWorkHours () {
+        echo $empCheck
+        if [ $empCheck -eq 1 ]
+        then
+        totalEmpWorkDays+=1;
+        fi
+        }
+
+
+for (( i=1;i<=$monthWorkingDays;i++))
+do
+        funResult="$(getWorkHours)"
+        echo $funResult
+
+case $empCheck in
+        $isPartTime)
+        empHours=4;
+        ;;
+        $isFullTime)
+        empHours=8;
+        ;;
+        *)
+        empHours=0
+        ;;
+esac
+dailyEmpWage=$(( $ratePerHour * $empHours))
+addingWage=$(($addingWage + $dailyEmpWage))
+done
+
+ if [ $totalEmpWorkDays -eq $monthWorkingDays ]
+then
+empWageForMonth=$(($addingWage * $monthWorkingDays))
+echo $empWageForMonth
+
+
+
+
+
+
+
+
+
